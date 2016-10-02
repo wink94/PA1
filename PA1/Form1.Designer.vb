67b7781d337a0bac99ffb8b6b5f0a58e6273105d
wink94 <mwindula16@gmail.com>
2016-10-02 14:37:37 +0530

    all

--- PA1/Form1.Designer.vb
+++ PA1/Form1.Designer.vb
@@ -22,9 +22,122 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblSide1 = New System.Windows.Forms.Label()
+        Me.lblSide2 = New System.Windows.Forms.Label()
+        Me.lblAngle = New System.Windows.Forms.Label()
+        Me.txtSide1 = New System.Windows.Forms.TextBox()
+        Me.txtSide2 = New System.Windows.Forms.TextBox()
+        Me.txtAngle = New System.Windows.Forms.TextBox()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(290, 234)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 0
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblSide1
+        '
+        Me.lblSide1.AutoSize = True
+        Me.lblSide1.Location = New System.Drawing.Point(120, 64)
+        Me.lblSide1.Name = "lblSide1"
+        Me.lblSide1.Size = New System.Drawing.Size(34, 13)
+        Me.lblSide1.TabIndex = 1
+        Me.lblSide1.Text = "Side1"
+        '
+        'lblSide2
+        '
+        Me.lblSide2.AutoSize = True
+        Me.lblSide2.Location = New System.Drawing.Point(316, 64)
+        Me.lblSide2.Name = "lblSide2"
+        Me.lblSide2.Size = New System.Drawing.Size(34, 13)
+        Me.lblSide2.TabIndex = 1
+        Me.lblSide2.Text = "Side2"
+        '
+        'lblAngle
+        '
+        Me.lblAngle.AutoSize = True
+        Me.lblAngle.Location = New System.Drawing.Point(539, 67)
+        Me.lblAngle.Name = "lblAngle"
+        Me.lblAngle.Size = New System.Drawing.Size(34, 13)
+        Me.lblAngle.TabIndex = 1
+        Me.lblAngle.Text = "Angle"
+        '
+        'txtSide1
+        '
+        Me.txtSide1.Location = New System.Drawing.Point(165, 64)
+        Me.txtSide1.Name = "txtSide1"
+        Me.txtSide1.Size = New System.Drawing.Size(100, 20)
+        Me.txtSide1.TabIndex = 2
+        '
+        'txtSide2
+        '
+        Me.txtSide2.Location = New System.Drawing.Point(361, 64)
+        Me.txtSide2.Name = "txtSide2"
+        Me.txtSide2.Size = New System.Drawing.Size(100, 20)
+        Me.txtSide2.TabIndex = 2
+        '
+        'txtAngle
+        '
+        Me.txtAngle.Location = New System.Drawing.Point(584, 64)
+        Me.txtAngle.Name = "txtAngle"
+        Me.txtAngle.Size = New System.Drawing.Size(100, 20)
+        Me.txtAngle.TabIndex = 2
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.AutoSize = True
+        Me.lblDisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDisplay.Location = New System.Drawing.Point(278, 147)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(72, 24)
+        Me.lblDisplay.TabIndex = 1
+        Me.lblDisplay.Text = "Label1"
+        Me.lblDisplay.Visible = False
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(440, 234)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 0
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(812, 380)
+        Me.Controls.Add(Me.txtAngle)
+        Me.Controls.Add(Me.txtSide2)
+        Me.Controls.Add(Me.txtSide1)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.lblAngle)
+        Me.Controls.Add(Me.lblSide2)
+        Me.Controls.Add(Me.lblSide1)
+        Me.Controls.Add(Me.btnClose)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblSide1 As Label
+    Friend WithEvents lblSide2 As Label
+    Friend WithEvents lblAngle As Label
+    Friend WithEvents txtSide1 As TextBox
+    Friend WithEvents txtSide2 As TextBox
+    Friend WithEvents txtAngle As TextBox
+    Friend WithEvents lblDisplay As Label
+    Friend WithEvents btnClose As Button
 End Class
--- PA1/Form1.vb
+++ PA1/Form1.vb
@@ -1,3 +1,44 @@
-﻿Public Class Form1
+﻿
 
+
+Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim side1, side2, side3, angle1_2, angle2_3, angle3_1 As Double
+        Dim displayString As String
+
+        If (String.IsNullOrEmpty(txtAngle.Text) = True Or String.IsNullOrEmpty(txtSide1.Text) = True Or String.IsNullOrEmpty(txtSide2.Text) = True) Then
+
+            lblDisplay.Text = "PLease enter values"
+
+        Else
+
+            side1 = Convert.ToDouble(txtSide1.Text)
+            side2 = Convert.ToDouble(txtSide2.Text)
+            angle1_2 = Convert.ToDouble(txtAngle.Text)
+
+            side3 = Math.Sqrt(-2 * side1 * side2 * Math.Cos(angle1_2 * Math.PI / 180) + side1 ^ 2 + side2 ^ 2)  'calculate front
+
+            If Double.NaN <> side3 Then
+
+                angle2_3 = (Math.Asin(side1 * Math.Sin(angle1_2 * Math.PI / 180) / side3)) * 180 / Math.PI
+                angle3_1 = 180 - (angle2_3 + angle1_2)
+
+                displayString = "angles " + FormatNumber(angle1_2, 2) + " , " + FormatNumber(angle2_3, 2) + " , " + FormatNumber(angle3_1, 2) + ",,,, sides  " + FormatNumber(side1, 2) + " , " + FormatNumber(side2, 2) + " , " + FormatNumber(side3, 2)
+
+                lblDisplay.Text = displayString
+                If lblDisplay.Visible = False Then
+                    lblDisplay.Visible = True
+                End If
+
+            End If
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        Dispose()
+    End Sub
 End Class
